3e55416f7c6cc803d7380a6c97a09020c9fb34f5
RakibulBh <rakibulbhuiyan1973@gmail.com>
2024-05-24 15:11:03 +0100

    middlware configs@

--- middleware.ts
+++ middleware.ts
@@ -2,18 +2,17 @@ import { type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  try {
+    console.log("Middleware request path:", request.nextUrl.pathname);
+    return await updateSession(request);
+  } catch (error) {
+    console.error("Middleware error:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
 }
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
-    // "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)).*)",
   ],
 };
